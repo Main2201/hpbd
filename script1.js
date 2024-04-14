2306b92422316a1dcab6b264c21d94f8a7eca3fa
Main2201 <ducchinh72201@gmail.com>
2024-04-14 23:58:44 +0700

    Add new project hpbd

---
+++ script1.js
@@ -0,0 +1,7 @@
+var um=prompt("Ai là em bé của Chính nàooo");
+var uk=prompt("Hôm nay là ngày gì vậy ta")
+    if(um=="Thương Huyền ạ"||um=="Ngô Thương Huyền ạ"||um=="Huyền ạ"||um=="Huynn ạ" && uk=="Sinh nhật bé ạ"||uk=="Sinh nhật Huynn ạ"||uk=="Sinh nhật Huyềnn ạ"||uk=="Sn Huynn ạ"||uk=="Sn Huyền ạ"||uk=="Sn bé ạ" ) {window.location.href="start.html"}
+    else{
+        alert("Sai rồi kìaa. Bé thử nhớ xem có thiếu gì hong 'ạ'");
+        window.location.href="index.html";
+    }
